5607b119cd7671b415bd37be9c047729d4f70108
ujangaripin24 <ujangarifin06@gmail.com>
2022-11-17 00:10:28 +0700

    2. login & register has been installed

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import LoginView from '../views/LoginView.vue'
+import RegisterView from '../views/Register/RegisterView.vue'
 
 Vue.use(VueRouter)
 
@@ -8,16 +9,13 @@ const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: LoginView
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+    path: '/register',
+    name: 'register',
+    component: RegisterView
+  },
 ]
 
 const router = new VueRouter({
